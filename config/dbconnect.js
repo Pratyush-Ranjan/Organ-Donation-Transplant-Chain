e91863cfaa4cf2b766f26e15e153f6ce7aa9d79a
Pratyush Ranjan Srivastava <38191977+Pratyush-Ranjan@users.noreply.github.com>
2019-07-02 17:51:31 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,6 +1,6 @@
 var mongoose= require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/OrganChain');
+mongoose.connect('mongodb://localhost/OrganChain');
 var db= mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
 db.once('open',function callback() {
