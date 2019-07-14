946b26d5b0de56a21b3b338da279561f4bbb13a0
Pratyush Ranjan Srivastava <38191977+Pratyush-Ranjan@users.noreply.github.com>
2019-07-15 02:32:39 +0530

    Update donor.js

--- models/donor.js
+++ models/donor.js
@@ -1,7 +1,6 @@
 var mongoose=require('mongoose');
 
 var userSchema= mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
     email: {type:String, required:true, unique:true,
         match:/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/},
     name: {type: String, required: true},
@@ -9,8 +8,7 @@ var userSchema= mongoose.Schema({
     contact: {type:String, required:true},
     city: {type : String, required: true},
     organ: {type: String, required:true},
-    bloodgroup: {type: String, required:true},
-    rhfactor:{type: String, required:true}
+    bloodgroup: {type: String, required:true}
 });
 
 module.exports=mongoose.model('donors',userSchema);
